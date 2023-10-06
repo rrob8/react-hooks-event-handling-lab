2dbcbf17d181bb969912c6745f62b186dacf36ab
Robert Bubnik <rbubnik@gmail.com>
2023-10-06 18:06:22 -0400

    passing all tests

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+import React from "react"
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+        <form>
+        <input 
+        type='password'
+        placeholder='Enter password here'
+        onChange={()=> console.log('Entering password...')}/>
+        </form>
+        </div>
     )
 }
 
